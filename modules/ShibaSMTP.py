bfb34ccc3b61c466dc57b743daf492dea88a5605
titosilva <jt.mat@hotmail.com>
2019-08-30 14:16:53 -0300

    Trabalhando no ShibaSMTP, junto ao ShibaDomains

--- modules/ShibaSMTP.py
+++ modules/ShibaSMTP.py
@@ -1,6 +1,14 @@
 import socket
+from ShibaDomains import IPAddress
 
 class SMTPStream(object):
-    def __init__(self, serveraddr, port):
+    def __init__(self, serveraddr='127.0.0.1', port=25):
         self.__serveraddr = serveraddr
-        self.__port = port
+        self.__smtpport = port
+    def setServerAddr(self, serveraddr):
+        #Verify if the given address is actually an IP address
+        if IPAddress.validateIP(serveraddr):
+            self.__serveraddr = serveraddr
+    def setPort(self, port):
+        if IPAddress.validatePort(port):
+            self.__smtpport = port
