9176a872b981a567936723966439cef72c46e5b0
titosilva <jt.mat@hotmail.com>
2019-09-01 09:38:21 -0300

    Working on ShibaSMTP module

--- modules/ShibaSMTP.py
+++ modules/ShibaSMTP.py
@@ -1,14 +1,38 @@
 import socket
 from ShibaDomains import IPAddress
 
-class SMTPStream(object):
-    def __init__(self, serveraddr='127.0.0.1', port=25):
+class SMTPMail(object):
+    def __init__(self, mailfrom: str, rcptto: str, serveraddr: str = '127.0.0.1', smtpport: int = 25):
+        self.__mailfrom = mailfrom
+        self.__rcptto = rcptto
         self.__serveraddr = serveraddr
-        self.__smtpport = port
-    def setServerAddr(self, serveraddr):
-        #Verify if the given address is actually an IP address
-        if IPAddress.validateIP(serveraddr):
-            self.__serveraddr = serveraddr
-    def setPort(self, port):
-        if IPAddress.validatePort(port):
-            self.__smtpport = port
+        self.__smtpport = smtpport
+
+    def setMailFrom(self, mailfrom: str):
+        self.__mailfrom = mailfrom
+    def setRcptTo(self, rcptto: str):
+        self.__rcptto = rcptto
+    def setServerAddr(self, serveraddr: str):
+        self.__serveraddr = serveraddr
+    def setSMTPPort(self, smtpport: int):
+        self.__smtpport = smtpport
+
+    def send(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
+            # Connects to the server
+            try:
+                tcp.connect((self.__serveraddr, self.__smtpport))
+            except:
+                print("Could not connect to server!")
+                return 0
+            received = tcp.recv(1024)
+            # Verifies if server Answer is fine
+            if not int(received[0:3]) == 220:
+                return 0
+            print(received)
+            tcp.send(b'quit')
+            tcp.close()
+
+if __name__ == "__main__":
+    teste = SMTPMail("ratchet@test.com", "ratchet@shibemail.com")
+    teste.send()
