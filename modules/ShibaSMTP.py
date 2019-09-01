b3b496d1d1529b5eb6154a49d4d8175aa6450c72
titosilva <jt.mat@hotmail.com>
2019-09-01 14:34:45 -0300

    Working on SMTP module

--- modules/ShibaSMTP.py
+++ modules/ShibaSMTP.py
@@ -1,12 +1,39 @@
 import socket
 
+class SMTPError(Exception):
+    pass
+
+class SMTPNoConnection(SMTPError):
+    pass
+
+class SMTPHeloError(SMTPError):
+    pass
+        
+class SMTPMailFromError(SMTPError):
+    pass
+
+class SMTPRcptToError(SMTPError):
+    pass
+
+class SMTPDataError(SMTPError):
+    pass
+
+class SMTPMsgError(SMTPError):
+    pass
+
+class SMTPPortError(SMTPError):
+    pass
+
 class SMTPMail(object):
-    def __init__(self, mailfrom: str, rcptto: str, serveraddr: str = '127.0.0.1', smtpport: int = 25):
+    # Constructor
+    def __init__(self, mailfrom: str, rcptto: str, serveraddr: str = '127.0.0.1', smtpport: int = 25, helo: str = 'shibemail.com'):
         self.__mailfrom = mailfrom
         self.__rcptto = rcptto
         self.__serveraddr = serveraddr
         self.__smtpport = smtpport
+        self.__helo = helo
 
+    # Functions that set private values
     def setMailFrom(self, mailfrom: str):
         self.__mailfrom = mailfrom
     def setRcptTo(self, rcptto: str):
@@ -14,24 +41,105 @@ class SMTPMail(object):
     def setServerAddr(self, serveraddr: str):
         self.__serveraddr = serveraddr
     def setSMTPPort(self, smtpport: int):
-        self.__smtpport = smtpport
+        if smtpport<65535 and smtpport>0:
+            self.__smtpport = smtpport
+        else:
+            raise SMTPPortError
 
-    def send(self):
+    # Functions that get private values
+    def getMailFrom(self):
+        return self.__mailfrom
+    def getRcptTo(self):
+        return self.__rcptto
+    def getServerAddr(self):
+        return self.__serveraddr
+    def getSMTPPort(self):
+        return self.__smtpport
+
+    # Send message contained in msg
+    def send(self, msg: str):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
-            # Connects to the server
+            # Tries to connect to the server
             try:
                 tcp.connect((self.__serveraddr, self.__smtpport))
             except:
-                print("Could not connect to server!")
-                return 0
+                tcp.close()
+                raise SMTPNoConnection
+            
             received = tcp.recv(1024)
+
             # Verifies if server Answer is fine
             if not int(received[0:3]) == 220:
-                return 0
-            print(received)
+                tcp.close()
+                raise SMTPError
+
+            print(1)
+
+            # Send Helo
+            tcp.send(b'helo shibemail.com')
+            received = tcp.recv(1024)
+
+            # Verifies if server Answer to Helo is fine
+            if not int(received[0:3]) == 250:
+                tcp.send(b'quit')
+                tcp.close()
+                raise SMTPHeloError
+
+            print(1)
+
+            # Send mail from
+            tcp.send(bytes('mail from: <' + self.__mailfrom + '>', encoding='ascii'))
+            received = tcp.recv(1024)
+
+            # Verifies if server Answer to mail from is fine
+            if not int(received[0:3]) == 250:
+                tcp.send(b'quit')
+                tcp.close()
+                raise SMTPMailFromError
+
+            print(1)
+
+            # Send rcpt to
+            tcp.send(bytes('rcpt to: <' + self.__rcptto + '>', encoding='ascii'))
+            received = tcp.recv(1024)
+
+            # Verifies if server Answer to rcpt tp is fine
+            if not int(received[0:3]) == 250:
+                tcp.send(b'quit')
+                tcp.close()
+                raise SMTPRcptToError
+
+            print(1)
+
+            # Send 'data'
+            tcp.send(b'data')
+            received = tcp.recv(1024)
+
+            # Verifies server answer to 'data'
+            if not int(received[0:3]) == 354:
+                tcp.send(b'quit')
+                tcp.close()
+                raise SMTPDataError
+
+            # Sends the message
+            tcp.send(bytes(msg + '\r\n.\r\n'))
+            received = tcp.recv(1024)
+            
+            # Verifies server answer to message sent
+            if not int(received[0:3]) == 250:
+                tcp.send(b'quit')
+                tcp.close()
+                raise SMTPMsgError
+
+            # Closes communication
             tcp.send(b'quit')
             tcp.close()
 
 if __name__ == "__main__":
-    teste = SMTPMail("ratchet@test.com", "ratchet@shibemail.com")
-    teste.send()
+    teste = SMTPMail(mailfrom = "ratchet@test.com", rcptto = "ratchet@shibemail.com")
+    try: 
+        teste.send('sent from shibasmtp mod')
+    except SMTPNoConnection:
+        print('Could not connect to server!')
+    except:
+        raise
