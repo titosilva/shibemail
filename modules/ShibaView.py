979bc3ec279e116ade1d51a9d0c8c84a7c72e313
titosilva <jt.mat@hotmail.com>
2019-09-11 11:21:43 -0300

    Little changes in the frontend

--- modules/ShibaView.py
+++ modules/ShibaView.py
@@ -21,7 +21,7 @@ def send_click():
     except ShibaSMTPError:
         tk.messagebox.showerror("Shibemail Error", "An Error ocurred")
     else:
-        print('mensagem enviada!')
+        tk.messagebox.showinfo("Mail sent successfully!")
 
 root = tk.Tk()
 root.title("Welcome to Shibemail :)")
@@ -53,13 +53,7 @@ ToLabel.place(relx=0.11,rely=0.365, relheight=0.05, relwidth=0.1)
 ToEntry = tk.Entry(root, font=40)
 ToEntry.place(relx=0.185,rely=0.37,relwidth=0.54, relheight=0.04)
 
-DescripitonLabel = tk.Label(root,text="Descripiton :",bg='#fce5ac',font=40)
-DescripitonLabel.place(relx=0.081,rely=0.435, relheight=0.05, relwidth=0.1)
-
-Descripiton= tk.Entry(root, font=40)
-Descripiton.place(relx=0.185,rely=0.44,relwidth=0.54, relheight=0.04)
-
-BodyLabel = tk.Label(root,text="Body :",bg='#fce5ac',font=40)
+BodyLabel = tk.Label(root,text="Mail :",bg='#fce5ac',font=40)
 BodyLabel.place(relx=0.1,rely=0.505, relheight=0.05, relwidth=0.1)
 
 Body= tk.Text(root, font=40 )
